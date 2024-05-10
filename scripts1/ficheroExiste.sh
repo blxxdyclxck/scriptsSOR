d71cc688ac5af960e0914245bdf6ddffefc6cbc4
blxxdyclxck <92039302+blxxdyclxck@users.noreply.github.com>
2024-05-10 10:12:53 +0200

    Delete scripts1 directory

--- scripts1/ficheroExiste.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -e "$1" ]; then
-    echo "El archivo '$1' existe en el sistema."
-else
-    echo "El archivo '$1' no existe en el sistema."
-fi
--- scripts1/usuarioExiste.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if id "$1" >/dev/null; then
-    echo "El usuario '$1' existe en el sistema."
-else
-    echo "El usuario '$1' no existe en el sistema."
-fi
